a356e886cc51d4e4a2f53d1de1df8c3966c6d9ea
Daniel Park <102764808+Parxd@users.noreply.github.com>
2023-02-08 15:20:11 -0600

    add simple movement

---
+++ Assets/Characters/Player/Jump.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Jump : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)] private float jumpHeight = 3f;
+    [SerializeField, Range(0, 5)] private int maxAirJumps = 0;
+    [SerializeField, Range(0f, 5f)] private float downwardMovementMultiplier = 3f;
+    [SerializeField, Range(0f, 5f)] private float upwardMovementMultiplier = 1.7f;
+
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    private GroundCheck ground;
+    private Controller control;
+
+    private int jumpPhase;
+    private float jumpSpeed;
+    private float defaultGravityScale;
+
+    private bool jumpRequest;
+    private bool onGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ground = GetComponent<GroundCheck>();
+        control = GetComponent<Controller>();
+        defaultGravityScale = 1f;
+    }
+
+    void Update()
+    {
+        jumpRequest |= control.input.RetrieveJumpInput();
+    }
+
+    private void FixedUpdate()
+    {
+        onGround = ground.GetGround();
+        velocity = rb.velocity;
+
+        if (onGround)
+        {
+            jumpPhase = 0;
+        }
+        if (jumpRequest)
+        {
+            jumpRequest = false;
+            JumpMovement();
+        }
+        if (rb.velocity.y > 0)
+        {
+            rb.gravityScale = upwardMovementMultiplier;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = downwardMovementMultiplier;
+        }
+        if (rb.velocity.y == 0)
+        {
+            rb.gravityScale = defaultGravityScale;
+        }
+        rb.velocity = velocity;
+    }
+
+    private void JumpMovement()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            ++jumpPhase;
+            jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight);
+            if (velocity.y > 0)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+            }
+            else if (velocity.y < 0)
+            {
+                jumpSpeed += Mathf.Abs(rb.velocity.y);
+            }
+            velocity.y = jumpSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/Move.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Move : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)] private float maxSpeed = 8f;
+    [SerializeField, Range(0f, 100f)] private float maxAcceleration = 35f;
+    [SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 20f;
+
+    private float maxSpeedChange;
+    private float acceleration;
+    private bool gCheck;
+
+    private Vector2 direction;
+    private Vector2 velocity;
+    private Vector2 desiredVelocity;
+
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    private GroundCheck ground;
+    private Controller control;
+    
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        ground = GetComponent<GroundCheck>();
+        control = GetComponent<Controller>();
+    }
+
+    void Update()
+    {
+        direction.x = control.input.RetrieveMoveInput();
+        desiredVelocity = new Vector2(direction.x, 0f) * Mathf.Max(maxSpeed - ground.friction, 0f);
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        gCheck = ground.onGround;
+        velocity = rb.velocity;
+        if (gCheck)
+        {
+            acceleration = maxAcceleration;
+        }
+        else
+        {
+            acceleration = maxAirAcceleration;
+        }
+        maxSpeedChange = acceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        rb.velocity = velocity;
+    }
+
+    private void Flip()
+    {
+        if (velocity.x < -0.01f)
+        {
+            sprite.flipX = true;
+        }
+        if (velocity.x > 0.01f)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
